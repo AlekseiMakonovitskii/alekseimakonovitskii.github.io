41c0985b62084d575573edf0cd88c8d76c7cd7bd
Aleksei Makonovicky <alexey.makonovicky@altcraft.com>
2025-04-04 18:52:33 +0300

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+const akPush = new AKPush();
+
+const $subscribeBtn = document.querySelector('#subscribe-btn');
+const $updatedBtn = document.querySelector('#update-btn')
+const $statusIndicator = document.querySelector('#status-indicator');
+
+const PushState = {
+    status: null
+}
+
+const PushStatusType = {
+    ok: 'ok',
+    fail: 'fail',
+}
+
+const StatusIndicatorClasses = {
+    active: 'active',
+    inactive: 'inactive',
+}
+
+async function onSubscribe() {
+    try {
+        await akPush.initSubscription();
+    } catch (error) {
+        console.log(error);
+    }
+
+    onUpdate();
+}
+
+async function onUpdate() {
+    try {
+        const response = await akPush.updateSubscription();
+        
+        PushState.status = response.status;
+    } catch (error) {
+        console.log(error);
+    }
+
+    renderStatusIndicator();
+}
+
+function renderStatusIndicator() {
+    if (PushState.status === PushStatusType.ok) {
+        $statusIndicator?.classList.remove(StatusIndicatorClasses.inactive);
+        $statusIndicator?.classList.add(StatusIndicatorClasses.active);
+    } else {
+        $statusIndicator?.classList.remove(StatusIndicatorClasses.active);
+        $statusIndicator?.classList.add(StatusIndicatorClasses.inactive);
+    }
+}
+
+onUpdate();
+
+$updatedBtn?.addEventListener('click', onUpdate);
+$subscribeBtn?.addEventListener('click', onSubscribe)
